814b6d84c456c22bccf3a89e7d7f202f27da9f74
disp <dispixxx@gmail.com>
2025-02-06 18:22:48 +0400

    celesma

---
+++ src/main/java/com/disp/learnspringsecurity/ContentController.java
@@ -0,0 +1,44 @@
+package com.disp.learnspringsecurity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContentController {
+
+    @GetMapping("/home")
+    public String handleWelcome() {
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String handleWelcome2() {
+        return "home";
+    }
+
+    @GetMapping("/admin/home")
+    public String handleAdminHome() {
+        return "home_admin";
+    }
+
+    @GetMapping("/user/home")
+    public String handleUserHome() {
+        return "home_user";
+    }
+
+    @GetMapping("/login")
+    public String handleLogin() {
+        return "custom_login";
+    }
+
+    @GetMapping("/hello")
+    public String handleGreeting() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String handleRegister() {
+        return "register";
+    }
+
+}
---
+++ src/main/java/com/disp/learnspringsecurity/SecurityConfiguration.java
@@ -0,0 +1,76 @@
+package com.disp.learnspringsecurity;
+
+import com.disp.learnspringsecurity.model.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .authorizeHttpRequests(registry->{
+                    registry.requestMatchers("/", "/hello", "register/**").permitAll();
+                    registry.requestMatchers("/admin/**").hasRole("ADMIN");
+                    registry.requestMatchers("/user/**").hasRole("USER");
+                    registry.anyRequest().authenticated();
+                })
+                .formLogin(httpSecurityFormLoginConfigurer -> {
+                    httpSecurityFormLoginConfigurer
+                            .loginPage("/login")
+                            .successHandler(new AuthenticationSuccessHandler())//Для РОЛИ USER -> user/home; Для РОЛИ ADMIN -> admin/home
+                            .permitAll();
+                })
+                .build();
+    }
+
+////    InMemoryUserAuth
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails normalUser = User.builder()
+//                .username("disp_user")
+//                .password("$2a$12$Bng6wA8tWEtOApi5sM5hHe8S6djyToC29oLFiNLFPneXbJyoTIvrS")//user
+//                .roles("USER")
+//                .build();
+//        UserDetails adminUser = User.builder()
+//                .username("disp_admin")
+//                .password("$2a$12$zG5AgbGNh2WO9bGEf0QqrO5PXub9WQSN009aoUL26VdhJtKMximqS")//admin
+//                .roles("ADMIN")
+//                .build();
+//        return new InMemoryUserDetailsManager(normalUser, adminUser);
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
