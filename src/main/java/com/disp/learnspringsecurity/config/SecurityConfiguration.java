132f3c151560ca6eb70015ef842539b65d12076a
disp <dispixxx@gmail.com>
2025-03-12 17:47:05 +0400

    background on index, reg, log pages

--- src/main/java/com/disp/learnspringsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/disp/learnspringsecurity/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -86,10 +87,10 @@ public class SecurityConfiguration {
                 .build();
     }
 
-//    @Bean
-//    WebSecurityCustomizer configureWebSecurity() {
-//        return (web) -> web.ignoring().requestMatchers("/image/**", "/js/**", "/css/**", "/webjars/**");
-//    }
+    @Bean
+    WebSecurityCustomizer configureWebSecurity() {
+        return (web) -> web.ignoring().requestMatchers("/image/**", "/js/**", "/css/**", "/webjars/**");
+    }
 
 /*//    InMemoryUserAuth
     @Bean
